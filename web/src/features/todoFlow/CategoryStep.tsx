ff4b2a9175e062114627ee337ae1cec4accd2c2d
Tsuni <zacharias.holm@gmail.com>
2022-11-09 21:24:08 +0100

    category step

--- web/src/features/todoFlow/CategoryStep.tsx
+++ web/src/features/todoFlow/CategoryStep.tsx
@@ -1,17 +1,19 @@
-import { map } from "lodash";
+import { filter, map } from "lodash";
 import { FlowButton } from "../../components/FlowButton";
+import { useTodoStore } from "../../helpers/store";
 import { Category } from "../../interfaces/category";
-import { categoryData } from "../../mockData/categoryData";
 
 interface Props {
     onSelectCategory: (category: Category) => void,
 }
 
 const CategoryStep = ({ onSelectCategory }: Props) => {
+    const categories  = useTodoStore(state => state.categories)
+    const onlyActivecategories = filter(categories, category => !category.isHidden);
 
     return (
         <div className="flex flex-col justify-center h-full space-y-8">
-            {map(categoryData, category => <FlowButton key={category.id} text={category.name} onClick={() => onSelectCategory(category)} />)}
+            {map(onlyActivecategories, category => <FlowButton key={category.id} text={category.name} onClick={() => onSelectCategory(category)} />)}
         </div>
     )
 }
