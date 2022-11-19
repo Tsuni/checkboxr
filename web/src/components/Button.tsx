6d971176de3f9ee42be29993441b9256d2f8b8d7
Tsuni <zacharias.holm@gmail.com>
2022-11-19 10:40:32 +0100

    better btn class

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -9,7 +9,7 @@ const Button = ({ onClick, text }: Props) => (
     <button
         onClick={onClick}
         type="button"
-        className="inline-flex items-center px-5 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-stone-700 hover:bg-stone-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-stone-500"
+        className="items-center px-5 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-stone-700 hover:bg-stone-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-stone-500"
       >
         {text}
       </button>
--- web/src/features/todoFlow/OneTimeTodoStep.tsx
+++ web/src/features/todoFlow/OneTimeTodoStep.tsx
@@ -9,9 +9,9 @@ const OneTimeTodoStep = ({ onTitleDone }: Props) => {
     const inputRef = useRef<HTMLInputElement>(null);
 
     return (
-        <div className="flex flex-col justify-center h-full">
-            <input ref={inputRef} placeholder="Add your description here..."
-                className="block w-full border-0 focus:border-0 bg-transparent resize-none focus:ring-0 outline-none"
+        <div className="flex flex-col justify-center h-full space-y-12">
+            <input ref={inputRef} placeholder="Title..."
+                className="block w-full border-0 focus:border-0 bg-transparent resize-none focus:ring-0 outline-none    "
             />
             <Button text="Next" onClick={() => onTitleDone(inputRef?.current?.value || '')} />
         </div>
