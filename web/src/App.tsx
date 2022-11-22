a781b6828c1c2778cc9988c000bb6d569a5968a9
Tsuni <zacharias.holm@gmail.com>
2022-11-22 20:29:07 +0100

    okayish sidebar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,7 @@
 import { Dialog, Transition } from '@headlessui/react';
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import './App.css';
-import { Dashboard } from './features/dashboard/Dashboard';
 import {
-  Bars3Icon,
   CalendarIcon,
   ChartBarIcon,
   FolderIcon,
@@ -13,157 +11,145 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { useTodoStore } from './helpers/store';
-import { Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
-function classNames(...classes :any) {
+function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
 }
 
 const navigation = [
-
-
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
+  { name: 'Home', to: '/', icon: HomeIcon, current: true },
+  { name: 'History', to: 'history', icon: UsersIcon, current: false },
+  { name: 'Analytics', to: 'analytics', icon: CalendarIcon, current: false },
+  { name: 'Settings', to: 'settings', icon: FolderIcon, current: false },
 ]
 
 const App = () => {
-  const { showSidebar, toggleSidebar } = useTodoStore(state => ({showSidebar: state.showSidebar, toggleSidebar: state.toggleSidebar}));
+  const { showSidebar, toggleSidebar } = useTodoStore(state => ({ showSidebar: state.showSidebar, toggleSidebar: state.toggleSidebar }));
   return (
     <>
-    <div className='h-full'>
-      <Transition.Root show={showSidebar} as={Fragment}>
-        <Dialog as="div" className="relative z-40 md:hidden" onClose={toggleSidebar}>
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 z-40 flex">
+      <div className='h-full'>
+        <Transition.Root show={showSidebar} as={Fragment}>
+          <Dialog as="div" className="relative z-40 md:hidden" onClose={toggleSidebar}>
             <Transition.Child
               as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
             >
-              <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-stone-200">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute top-0 right-0 -mr-12 pt-2">
-                    <button
-                      type="button"
-                      className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-stone-700"
-                      onClick={() => toggleSidebar()}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XMarkIcon className="h-6 w-6 text-stone-700" aria-hidden="true" />
-                    </button>
-                  </div>
-                </Transition.Child>
-                <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
-                  <div className="flex flex-shrink-0 items-center px-4">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt="Your Company"
-                    />
-                  </div>
-                  <nav className="mt-5 space-y-1 px-2">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-stone-300 text-stone-700'
-                            : 'text-stone-700 hover:bg-stone-300 hover:text-stone-700',
-                          'group flex items-center px-2 py-2 text-base font-medium rounded-md'
-                        )}
-                      >
-                        <item.icon
-                          className={classNames(
-                            item.current ? 'text-stone-700' : 'text-stone-700 group-hover:text-stone-700',
-                            'mr-4 flex-shrink-0 h-6 w-6'
-                          )}
-                          aria-hidden="true"
-                        />
-                        {item.name}
-                      </a>
-                    ))}
-                  </nav>
-                </div>
-              </Dialog.Panel>
+              <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
             </Transition.Child>
-            <div className="w-14 flex-shrink-0">{/* Force sidebar to shrink to fit close icon */}</div>
-          </div>
-        </Dialog>
-      </Transition.Root>
 
-      {/* Static sidebar for desktop */}
-      <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
-        {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex min-h-0 flex-1 flex-col border-r border-stone-300 bg-stone-200">
-          <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
-            <div className="flex flex-shrink-0 items-center px-4">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-              />
+            <div className="fixed inset-0 z-40 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-stone-200">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute top-0 right-0 -mr-12 pt-2">
+                      <button
+                        type="button"
+                        className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-stone-700"
+                        onClick={() => toggleSidebar()}
+                      >
+                        <span className="sr-only">Close sidebar</span>
+                        <XMarkIcon className="h-6 w-6 text-stone-700" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
+                    <div className="flex flex-shrink-0 items-center px-4">
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Checkboxr"
+                      />
+                    </div>
+                    <nav className="mt-5 space-y-1 px-2">
+                      {navigation.map((item) => (
+                        <NavLink
+                          key={item.name}
+                          to={item.to}
+                          className='text-stone-700 hover:bg-stone-300 hover:text-stone-700 group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                        >
+                          <item.icon
+                            className={classNames(
+                              item.current ? 'text-stone-700' : 'text-stone-700 group-hover:text-stone-700',
+                              'mr-4 flex-shrink-0 h-6 w-6'
+                            )}
+                            aria-hidden="true"
+                          />
+                          {item.name}
+                        </NavLink>
+                      ))}
+                    </nav>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+              <div className="w-14 flex-shrink-0">{/* Force sidebar to shrink to fit close icon */}</div>
             </div>
-            <nav className="mt-5 flex-1 space-y-1 bg-stone-200 px-2">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-stone-300 text-stone-700' : 'text-stone-500 hover:bg-gray-50 hover:text-gray-900',
-                    'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-                  )}
-                >
-                  <item.icon
-                    className={classNames(
-                      item.current ? 'text-stone-700' : 'text-stone-500 group-hover:text-gray-500',
-                      'mr-3 flex-shrink-0 h-6 w-6'
-                    )}
-                    aria-hidden="true"
-                  />
-                  {item.name}
-                </a>
-              ))}
-            </nav>
+          </Dialog>
+        </Transition.Root>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex min-h-0 flex-1 flex-col border-r border-stone-300 bg-stone-200">
+            <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
+              <div className="flex flex-shrink-0 items-center px-4">
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt="Checkboxr"
+                />
+              </div>
+              <nav className="mt-5 flex-1 space-y-1 bg-stone-200 px-2">
+                {navigation.map((item) => (
+                  <NavLink
+                    key={item.name}
+                    to={item.to}
+                    className='text-stone-700 hover:bg-gray-50 hover:text-stone-700 group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                  >
+                    <item.icon
+                      className={classNames(
+                        item.current ? 'text-stone-700' : 'text-stone-700 group-hover:text-gray-500',
+                        'mr-3 flex-shrink-0 h-6 w-6'
+                      )}
+                      aria-hidden="true"
+                    />
+                    {item.name}
+                  </NavLink>
+                ))}
+              </nav>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex flex-1 flex-col md:pl-64 h-full">
+        <div className="flex flex-1 flex-col md:pl-64 h-full">
           <main className="flex-1 h-full">
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 py-6 h-full">
-                <Outlet />
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 py-6 h-full">
+              <Outlet />
             </div>
           </main>
         </div>
-    </div>
-  </>
+      </div>
+    </>
   )
 }
 
