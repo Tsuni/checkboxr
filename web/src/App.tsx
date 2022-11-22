d64441b84fb4f640f6bd45cabbaf63f87e60a00e
Tsuni <zacharias.holm@gmail.com>
2022-11-22 19:55:33 +0100

    working desktop sidebar version

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -34,14 +34,6 @@ const App = () => {
   const { showSidebar, toggleSidebar } = useTodoStore(state => ({showSidebar: state.showSidebar, toggleSidebar: state.toggleSidebar}));
   return (
     <>
-    {/*
-      This example requires updating your template:
-
-      ```
-      <html class="h-full bg-gray-100">
-      <body class="h-full">
-      ```
-    */}
     <div className='h-full'>
       <Transition.Root show={showSidebar} as={Fragment}>
         <Dialog as="div" className="relative z-40 md:hidden" onClose={toggleSidebar}>
@@ -67,7 +59,7 @@ const App = () => {
               leaveFrom="translate-x-0"
               leaveTo="-translate-x-full"
             >
-              <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-white">
+              <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-stone-200">
                 <Transition.Child
                   as={Fragment}
                   enter="ease-in-out duration-300"
@@ -80,11 +72,11 @@ const App = () => {
                   <div className="absolute top-0 right-0 -mr-12 pt-2">
                     <button
                       type="button"
-                      className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                      className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-stone-700"
                       onClick={() => toggleSidebar()}
                     >
                       <span className="sr-only">Close sidebar</span>
-                      <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                      <XMarkIcon className="h-6 w-6 text-stone-700" aria-hidden="true" />
                     </button>
                   </div>
                 </Transition.Child>
@@ -103,14 +95,14 @@ const App = () => {
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? 'bg-gray-100 text-gray-900'
-                            : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                            ? 'bg-stone-300 text-stone-700'
+                            : 'text-stone-700 hover:bg-stone-300 hover:text-stone-700',
                           'group flex items-center px-2 py-2 text-base font-medium rounded-md'
                         )}
                       >
                         <item.icon
                           className={classNames(
-                            item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
+                            item.current ? 'text-stone-700' : 'text-stone-700 group-hover:text-stone-700',
                             'mr-4 flex-shrink-0 h-6 w-6'
                           )}
                           aria-hidden="true"
@@ -119,23 +111,6 @@ const App = () => {
                       </a>
                     ))}
                   </nav>
-                </div>
-                <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-                  <a href="#" className="group block flex-shrink-0">
-                    <div className="flex items-center">
-                      <div>
-                        <img
-                          className="inline-block h-10 w-10 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </div>
-                      <div className="ml-3">
-                        <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">Tom Cook</p>
-                        <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-                      </div>
-                    </div>
-                  </a>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
@@ -147,7 +122,7 @@ const App = () => {
       {/* Static sidebar for desktop */}
       <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
         {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white">
+        <div className="flex min-h-0 flex-1 flex-col border-r border-stone-300 bg-stone-200">
           <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
             <div className="flex flex-shrink-0 items-center px-4">
               <img
@@ -156,19 +131,19 @@ const App = () => {
                 alt="Your Company"
               />
             </div>
-            <nav className="mt-5 flex-1 space-y-1 bg-white px-2">
+            <nav className="mt-5 flex-1 space-y-1 bg-stone-200 px-2">
               {navigation.map((item) => (
                 <a
                   key={item.name}
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                    item.current ? 'bg-stone-300 text-stone-700' : 'text-stone-500 hover:bg-gray-50 hover:text-gray-900',
                     'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
                   )}
                 >
                   <item.icon
                     className={classNames(
-                      item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
+                      item.current ? 'text-stone-700' : 'text-stone-500 group-hover:text-gray-500',
                       'mr-3 flex-shrink-0 h-6 w-6'
                     )}
                     aria-hidden="true"
@@ -178,31 +153,12 @@ const App = () => {
               ))}
             </nav>
           </div>
-          <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-            <a href="#" className="group block w-full flex-shrink-0">
-              <div className="flex items-center">
-                <div>
-                  <img
-                    className="inline-block h-9 w-9 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-3">
-                  <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">Tom Cook</p>
-                  <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-                </div>
-              </div>
-            </a>
-          </div>
         </div>
       </div>
-      <div className="flex flex-1 flex-col md:pl-64">
-          <main className="flex-1">
-            <div className="py-6">
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+      <div className="flex flex-1 flex-col md:pl-64 h-full">
+          <main className="flex-1 h-full">
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 py-6 h-full">
                 <Outlet />
-              </div>
             </div>
           </main>
         </div>
